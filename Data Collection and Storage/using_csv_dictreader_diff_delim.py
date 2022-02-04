b192658d74dc924ef25bf86abf2a11759d11c6ec
Ricky Mitchell <ramitch91@gmail.com>
2022-02-04 10:44:03 -0600

    add different delimeters

---
+++ Data Collection and Storage/using_csv_dictreader_diff_delim.py
@@ -0,0 +1,18 @@
+import csv
+import os
+
+cwd = os.getcwd()
+os.chdir(cwd + "/Data Collection and Storage")
+
+with open("different_delim.csv") as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter="|")
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        print(
+            f'\t{row["name"]} lives at {row["address"]}, and joined on {row["date joined"]}'
+        )
+        line_count += 1
+    print(f"Processed {line_count} lines.")
