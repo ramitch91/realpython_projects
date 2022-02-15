ed2855df5050cf638475b5893c02d3f5a7daa053
Ricky Mitchell <ramitch91@gmail.com>
2022-02-14 21:39:59 -0600

    create a json object to capture information about  sample.xlsx

---
+++ Data Collection and Storage/Excel Spreadsheets in Python/parse_products.py
@@ -0,0 +1,22 @@
+import json
+import os
+from openpyxl import load_workbook
+
+cwd = os.getcwd()
+os.chdir(cwd + "/Data Collection and Storage/Excel Spreadsheets in Python")
+
+workbook = load_workbook(filename="sample.xlsx")
+sheet = workbook.active
+
+products = {}
+
+for row in sheet.iter_rows(min_row=2, min_col=4, max_col=7, values_only=True):
+    product_id = row[0]
+    product = {
+        "parent": row[1],
+        "title": row[2],
+        "category": row[3],
+    }
+    products[product_id] = product
+
+print(json.dumps(products))
