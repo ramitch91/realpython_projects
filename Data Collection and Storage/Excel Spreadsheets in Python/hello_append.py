555ad13f7170e6a88f64c8ebeba2af8582389ca9
Ricky Mitchell <ramitch91@gmail.com>
2022-02-19 05:06:39 -0600

    add file to write to a spreadsheet

--- Data Collection and Storage/Excel Spreadsheets in Python/hello_append.py
+++ Data Collection and Storage/Excel Spreadsheets in Python/hello_append.py
@@ -1,6 +1,11 @@
+import os
+
 from openpyxl import load_workbook
 
-workbook = load_workbook(filename="hello_world.xlsx")
+cwd = os.getcwd()
+os.chdir(cwd + "/Data Collection and Storage/Excel Spreadsheets in Python")
+
+workbook = load_workbook("hello_world.xlsx")
 sheet = workbook.active
 
 sheet["C1"] = "writing!"
