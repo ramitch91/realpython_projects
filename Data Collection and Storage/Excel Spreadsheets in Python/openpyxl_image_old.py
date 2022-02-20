fee3cad449ce0dee977d21ca70f9725b640b559e
Ricky Mitchell <ramitch91@gmail.com>
2022-02-20 05:52:46 -0600

    add an image to the spreadsheet

--- Data Collection and Storage/Excel Spreadsheets in Python/openpyxl_image_old.py
+++ Data Collection and Storage/Excel Spreadsheets in Python/openpyxl_image_old.py
@@ -1,14 +1,11 @@
-import os
 from openpyxl import load_workbook
 from openpyxl.drawing.image import Image
 
-cwd = os.getcwd()
-os.chdir(cwd + "/Data Collection and Storage/Excel Spreadsheets in Python")
-
-workbook = load_workbook("hello_world.xlsx")
+workbook = load_workbook(filename="hello_world.xlsx")
 sheet = workbook.active
 
 logo = Image("rp.png")
+
 logo.height = 150
 logo.width = 150
 
