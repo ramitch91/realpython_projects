da104d34625c69570477b82bee68998a36677f79
Ricky Mitchell <ramitch91@gmail.com>
2022-02-22 20:48:11 -0600

    add conversions from spreadsheets and dataframes

---
+++ Data Collection and Storage/Excel Spreadsheets in Python/df_excel.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+
+cwd = os.getcwd()
+os.chdir(cwd + "/Data Collection and Storage/Excel Spreadsheets in Python")
+
+data = {
+    "Product Name": ["Product 1", "Product 2"],
+    "Sales Month 1": [10, 20],
+    "Sales Month 2": [5, 35],
+}
+
+df = pd.DataFrame(data)
+
+# convert dataframe to worksheet
+
+workbook = Workbook()
+sheet = workbook.active
+
+for row in dataframe_to_rows(df, index=False, header=True):
+    sheet.append(row)
+
+workbook.save("pandas.xlsx")
--- Data Collection and Storage/Excel Spreadsheets in Python/excel_df.py
+++ Data Collection and Storage/Excel Spreadsheets in Python/excel_df.py
@@ -1,41 +1,23 @@
-# import pandas as pd
-# from openpyxl import Workbook
-# from openpyxl.utils.dataframe import dataframe_to_rows
-
-# data = {
-#     "Product Name": ["Product 1", "Product 2"],
-#     "Sales Month 1": [10, 20],
-#     "Sales Month 2": [5, 35],
-# }
-
-# df = pd.DataFrame(data)
-
-# ### convert dataframe to worksheet
-
-# workbook = Workbook()
-# sheet = workbook.active
-
-# for row in dataframe_to_rows(df, index=False, header=True):
-#     sheet.append(row)
-
-# workbook.save("pandas.xlsx")
-
+from dataclasses import dataclass
+import os
 import pandas as pd
 from openpyxl import load_workbook
 from mapping import REVIEW_ID
+
+cwd = os.getcwd()
+os.chdir(cwd + "/Data Collection and Storage/Excel Spreadsheets in Python")
 
 workbook = load_workbook(filename="sample.xlsx")
 sheet = workbook.active
 
 data = sheet.values
 
-# set the first row as the headers
+# set the first row as te headers
 cols = next(data)
 data = list(data)
 
-# set index
+# set the index
 idx = [row[REVIEW_ID] for row in data]
-
 
 df = pd.DataFrame(data, index=idx, columns=cols)
 
