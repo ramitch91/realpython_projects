3b44f9b939ab56835096b26d8e6e644a8b75bafc
Ricky Mitchell <ramitch91@gmail.com>
2022-04-17 18:29:45 -0500

    add ability to see image in tkgui

--- tkgui/tk_image.py
+++ tkgui/tk_image.py
@@ -2,10 +2,10 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 root = Tk()
-root.title('Test Image')
-# root.iconbitmap('world.ico')
+root.title("Test Image")
+# root.iconbitmap("/home/ricky/Documents/code/tkgui/world.ico")
 
-my_img = ImageTk.PhotoImage(Image, open('/home/ricky/Documents/code/tkgui/taffy5.jpg'))
+my_img = ImageTk.PhotoImage(Image.open("/home/ricky/Documents/code/tkgui/taffy5.jpg"))
 my_label = Label(root, image=my_img)
 my_label.pack()
 
